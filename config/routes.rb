b0284de186536f14d0046e7bd7c26edefde860b9
taji <taji@sonicgarden.jp>
2025-08-17 16:23:29 +0900

    管理者root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :administrators, controllers: { sessions: 'administrators/sessions' }
 
+  namespace :administrators do
+    root 'home#index'
+  end
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
