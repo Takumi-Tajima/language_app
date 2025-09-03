6d9042ab5523a758d2900099c158ab15160f81cc
taji <taji@sonicgarden.jp>
2025-09-03 21:15:22 +0900

    レッスンチケットを作成

--- app/controllers/administrators/purchases_controller.rb
+++ app/controllers/administrators/purchases_controller.rb
@@ -1,11 +1,16 @@
 class Administrators::PurchasesController < Administrators::ApplicationController
-  before_action :set_purchase, only: %i[show]
+  before_action :set_purchase, only: %i[show update]
 
   def index
     @purchases = Purchase.includes(:user).default_order
   end
 
   def show
+  end
+
+  def update
+    @purchase.paid! unless @purchase.paid?
+    redirect_to administrators_purchase_path(@purchase), notice: '支払いステータスを変更しました。'
   end
 
   private
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -9,6 +9,8 @@ class Purchase < ApplicationRecord
   validates :tax_amount, presence: true, numericality: { only_integer: true }
   validates :subtotal, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
+  after_update :create_lesson_ticket, if: :should_create_lesson_ticket?
+
   scope :default_order, -> { order(created_at: :desc) }
   scope :paid, -> { where.not(paid_at: nil) }
   scope :unpaid, -> { where(paid_at: nil) }
@@ -17,11 +19,17 @@ class Purchase < ApplicationRecord
     update!(paid_at: Time.current)
   end
 
-  def unpaid!
-    update!(paid_at: nil)
-  end
-
   def paid?
     paid_at.present?
+  end
+
+  private
+
+  def should_create_lesson_ticket?
+    saved_change_to_paid_at? && paid?
+  end
+
+  def create_lesson_ticket
+    LessonTicket.create!(user: user, purchase: self, ticket_type: ticket_type)
   end
 end
