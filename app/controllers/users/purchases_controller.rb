3057f93e3cc1edb0c93d79b8548ba10355046ad8
taji <taji@sonicgarden.jp>
2025-09-03 11:36:19 +0900

    購入手続き処理をUserモデルへ

--- app/controllers/users/purchases_controller.rb
+++ app/controllers/users/purchases_controller.rb
@@ -8,14 +8,7 @@ class Users::PurchasesController < ApplicationController
 
   def create
     ticket_type = params.expect(:ticket_type)
-
-    purchase = current_user.purchases.create!(
-      ticket_type: ticket_type,
-      total_amount: TaxRate.calculate_total_amount(LessonTicket::TICKET_INFO[ticket_type.to_sym][:price]),
-      tax_amount: TaxRate.calculate_tax_amount(LessonTicket::TICKET_INFO[ticket_type.to_sym][:price]),
-      subtotal: LessonTicket::TICKET_INFO[ticket_type.to_sym][:price]
-    )
-
+    purchase = current_user.create_purchase!(ticket_type)
     redirect_to users_purchase_path(purchase), notice: '購入手続きをしました。メールをご確認ください', status: :see_other
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
 
   has_many :purchases, dependent: :destroy
   has_many :lesson_tickets, dependent: :destroy
+
+  def create_purchase!(ticket_type)
+    ticket_price = LessonTicket::TICKET_INFO[ticket_type.to_sym][:price]
+
+    total_amount = TaxRate.calculate_total_amount(ticket_price)
+    tax_amount = TaxRate.calculate_tax_amount(ticket_price)
+    subtotal = ticket_price
+
+    purchases.create!(
+      ticket_type: ticket_type,
+      total_amount: total_amount,
+      tax_amount: tax_amount,
+      subtotal: subtotal
+    )
+  end
 end
