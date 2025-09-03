ef113a111f849860a78996747017852b0ec2ebed
taji <taji@sonicgarden.jp>
2025-09-03 14:04:06 +0900

    リファクタ

--- app/controllers/users/purchases_controller.rb
+++ app/controllers/users/purchases_controller.rb
@@ -1,10 +1,11 @@
 class Users::PurchasesController < ApplicationController
+  before_action :set_purchase, only: %i[show]
+
   def index
     @purchases = current_user.purchases.default_order
   end
 
   def show
-    @purchase = current_user.purchases.find(params.expect(:id))
   end
 
   def new
@@ -13,7 +14,13 @@ class Users::PurchasesController < ApplicationController
   def create
     ticket_type = params.expect(:ticket_type)
     purchase = current_user.create_purchase!(ticket_type)
-    PurchaseMailer.transfer_request_email(purchase, current_user).deliver_later
+    PurchaseMailer.request_transfer_email(purchase, current_user).deliver_later
     redirect_to users_purchase_path(purchase), notice: '購入手続きをしました。メールをご確認ください', status: :see_other
+  end
+
+  private
+
+  def set_purchase
+    @purchase = current_user.purchases.find(params.expect(:id))
   end
 end
--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,5 +1,5 @@
 class PurchaseMailer < ApplicationMailer
-  def transfer_request_email(purchase, user)
+  def request_transfer_email(purchase, user)
     @purchase = purchase
     @user = user
     mail(to: @user.email, subject: '【購入手続きについて】お買い上げありがとうございます')
