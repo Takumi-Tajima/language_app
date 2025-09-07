bfa1fd49752f22c895be1c318667efd37dd176eb
taji <taji@sonicgarden.jp>
2025-09-07 18:50:14 +0900

    予約がある場合はスケジュールの変更はできない

--- app/models/lesson_schedule.rb
+++ app/models/lesson_schedule.rb
@@ -10,6 +10,7 @@ class LessonSchedule < ApplicationRecord
   validate :validate_no_overlap_lesson_schedules
 
   before_validation :set_end_at
+  before_update :do_not_update_start_at_if_booking_present
   before_destroy :do_not_destroy_if_booking_present
 
   scope :default_order, -> { order(:start_at) }
@@ -42,7 +43,14 @@ class LessonSchedule < ApplicationRecord
 
   def do_not_destroy_if_booking_present
     if is_booked?
-      errors.add(:is_booked, '予約が存在するため削除できません')
+      errors.add(:base, '予約が存在するため削除できません')
+      throw(:abort)
+    end
+  end
+
+  def do_not_update_start_at_if_booking_present
+    if is_booked?
+      errors.add(:base, '予約が存在するため、開始時刻の変更はできません。ユーザーと直接やりとりをしてください。')
       throw(:abort)
     end
   end
